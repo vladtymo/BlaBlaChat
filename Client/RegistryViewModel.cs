ca1d0713a674eae6fb07354bb77c86510d2e05ce
anatolijkotok@gmail.com <anatolijkotok@gmail.com>
2021-03-29 13:24:29 +0300

    Creat registry view model

---
+++ Client/RegistryViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+
+namespace Client
+{
+    public class RegistryViewModel:ViewModelBase
+    {
+        private ICollection<string> months = new ObservableCollection<string>();
+        public IEnumerable<string> Months => months;
+        public RegistryViewModel()
+        {
+            months = new ObservableCollection<string>(DateTimeFormatInfo.CurrentInfo.MonthNames); 
+        }
+        private string? _mail;
+
+
+        public string? Mail
+        {
+            get => _mail;
+            set => SetProperty(ref _mail, value);
+        }
+        private string? _name;
+        public string? Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+        private string? _surname;
+        public string? Surname
+        {
+            get => _surname;
+            set => SetProperty(ref _surname, value);
+        }
+        private string? _month;
+        public string? Month
+        {
+            get => _month;
+            set => SetProperty(ref _month, value);
+        }
+        private string? _day;
+        public string? Day
+        {
+            get => _day;
+            set => SetProperty(ref _day, value);
+        }
+        private string? _year;
+        public string? Year
+        {
+            get => _year;
+            set => SetProperty(ref _year, value);
+        }
+        private DateTime? _validatingDate;
+        public DateTime? ValidatingDate
+        {
+            get => _validatingDate;
+            set => SetProperty(ref _validatingDate, value);
+        }
+    }
+}
