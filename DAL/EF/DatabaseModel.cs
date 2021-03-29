f48d576c0b3ff0276aa33c74135d479aa765629e
Микола Селецький <Seletskyi.Mykola@outlook.com>
2021-03-29 23:53:24 +0300

    Revert "Delete"
    This reverts commit e77742aced4755cd672030f29f428400a96af87d.

---
+++ DAL/EF/DatabaseModel.cs
@@ -0,0 +1,20 @@
+namespace Dal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class DatabaseModel : DbContext
+    {
+        public DatabaseModel()
+            : base("Data Source=den1.mssql7.gear.host;Initial Catalog=blablachatdb;Persist Security Info=True;User ID=blablachatdb;Password=Zo848Ut-j87~")
+        {
+            Database.SetInitializer(new Initializer());
+        }
+        public virtual DbSet<Chat> Chats { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Messages> Messages { get; set; }
+    }
+
+}
---
+++ DAL/EF/Initializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Dal
+{
+    internal class Initializer : DropCreateDatabaseIfModelChanges<DatabaseModel>
+    {
+        protected override void Seed(DatabaseModel context)
+        {
+            base.Seed(context);
+        }
+    }
+}
